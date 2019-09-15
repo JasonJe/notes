62e2d83be8ebd39d45532c47dd6d11ae1dc6a853
jasonje <jasonje@foxmail.com>
2019-09-15 23:06:01 +0800

    CI 工具增加创建目录，创建新文档部分

--- ci.py
+++ ci.py
@@ -44,14 +44,6 @@ def get_doc_detail(id_or_namespace, slug_or_id):
     response = requests.request("GET", url, headers=headers)
     return response.json()
 
-def get_github_repo_doc(route):
-    url = 'https://raw.githubusercontent.com/JasonJe/notes/master/机器学习/监督学习.md'
-    headers = {
-        'Authorization': 'token ' + github_token
-    }
-    response = requests.request("GET", url, headers=headers)
-    return response.text
-
 def create_new_doc(id_or_namespace, title, body, slug = "", public = 0, format_ = "markdown"):
     data = "title=%s&slug=%s&public=%s&format=%s&body=%s" % (urllib.parse.quote(title), slug, str(public), format_, urllib.parse.quote(body))
     url = api_url + '/repos/' + id_or_namespace + '/docs'
@@ -62,7 +54,17 @@ def create_new_doc(id_or_namespace, title, body, slug = "", public = 0, format_ 
     response = requests.request("POST", url, headers=headers, data=data, timeout=5000)
     return response.json()
 
-def get_toc_data(id_or_namespace):
+def update_doc(id_or_namespace, doc_id, title, body, slug = "", public = 0):
+    data = "title=%s&slug=%s&public=%s&body=%s" % (urllib.parse.quote(title), slug, str(public), urllib.parse.quote(body))
+    url = api_url + '/repos/' + id_or_namespace + '/docs/' + doc_id
+    headers = {
+        'x-auth-token': token,
+        'content-type': "application/x-www-form-urlencoded",
+    }
+    response = requests.request("PUT", url, headers=headers, data=data, timeout=5000)
+    return response.json()
+
+def get_repo_toc_data(id_or_namespace):
     url = api_url + '/repos/' + id_or_namespace + '/toc'
     headers = {
         'x-auth-token': token,
@@ -71,11 +73,58 @@ def get_toc_data(id_or_namespace):
     response = requests.request("GET", url, headers=headers)
     return response.json()
 
+def generate_toc_text(toc_data):
+    template = '{}- [{}]({})\n'
+    result = ''
+    for i in toc_data:
+        result += template.format(" " * (i['depth'] - 1) * 2, i['title'], i['slug'] if i['slug'] != "#" else "")
+    return result
+
+def update_repos_toc(id_or_namespace, toc_text):
+    data = "toc=%s" % (urllib.parse.quote(toc_text))
+    url = api_url + '/repos/' + id_or_namespace
+    headers = {
+        'x-auth-token': token,
+        'content-type': "application/x-www-form-urlencoded",
+    }
+    response = requests.request("PUT", url, headers=headers, data=data, timeout=5000)
+    return response.json()
+
+def generate_toc_data(index_file):
+    with open(index_file, 'r') as f:
+        index_body = f.read()
+    doc_list = []
+    for i in re.findall('(\d\..*<\/strong><\/summary>|#{4}.*)', index_body):
+        if '</strong></summary>' in i:
+            doc_list.append({
+                'slug': i[0],
+                'depth': 1,
+                'title': i.replace('</strong></summary>', ''),
+                'file_path': ''
+            })
+        else:
+            slug_repx = re.match('\d+\.\d+', i.replace('#### ', ''))
+            if slug_repx is not None:
+                file_path_repx = re.match('.*\.md', i)
+                slug = i.replace('#### ', '')[slug_repx.span()[0] : slug_repx.span()[1]]
+                doc_list.append({
+                    'slug': slug,
+                    'depth': 2,
+                    'title': slug + ' ' + i.replace('#### ', '').split('[')[1].split(']')[0].replace('`', '') if '[' in i else i.replace('#### ', '').replace('`', ''),
+                    'file_path': os.path.join(path, i[file_path_repx.span()[0]: file_path_repx.span()[1]].rsplit('(')[-1]) if file_path_repx is not None else ''
+                })
+    return doc_list[:-1]
+
+def diff_repo_and_local(docs_list, toc_data):
+    doc_set = set([i['title'] for i in docs_list])
+    toc_set = set([i['title'] for i in toc_data if i['depth'] != 1])
+    return toc_set - doc_set
+
 def list_mkdfiles():
     files = []
     for i in os.walk(path):
         if i[2] != []:
-            files.extend([os.path.join(i[0], j) for j in i[2] if j.rsplit(r'.')[-1].lower() == 'md'])
+            files.extend([os.path.join(i[0], j) for j in i[2] if j.rsplit(r'.')[-1].lower() == 'md' and 'README' not in j])
     return files
 
 def list_commit_hexsha():
@@ -99,18 +148,38 @@ def load_hexsha():
     return head_hexsha
 
 if __name__ == "__main__":
-    repos = get_repos("/users/jasonje/repos")
-    for repo in repos:
-        repo_detail = get_repo_detail(repo['namespace'])
-        # pprint.pprint(repo_detail)
-        docs_list = get_repo_docs_list(str(repo['id']))
-        # pprint.pprint(docs_list)
-        if repo['name'] == 'NOTES':
-            pprint.pprint(repo_detail['toc'])
-            pprint.pprint(docs_list)
-            # pprint.pprint(get_toc_data(str(repo['id'])))
-        # if repo['name'] == "测试":
-        #     pprint.pprint(docs_list)
-        #     if docs_list == []:
-        #         for i in r.iter_commits():
-        #             print(i.message) 
+    index_file = os.path.join(path, 'README.md')
+    toc_data = generate_toc_data(index_file)
+    toc_text = generate_toc_text(toc_data)
+    
+    repo_id = "417925"
+    docs_list = get_repo_docs_list(repo_id)
+    diff_docs = diff_repo_and_local(docs_list, toc_data)
+    print('Diff doc between repo and local is: ', diff_docs) 
+    
+    for i in toc_data:
+        if i['title'] in diff_docs and i['file_path'] != '':
+            body = ''
+            with open(i['file_path'], 'r') as f:
+                body = f.read()
+            resp = create_new_doc(repo_id, i['title'], body, slug = i['slug'], public = 0)
+            print('Create new doc response: ', resp)
+
+    repo_hexsha = load_hexsha()
+    update_files = diff_commit_files(repo_hexsha)
+
+    update_files_slug = []
+    for i in toc_data:
+        if i['file_path'] in update_files:
+            update_files_slug.append(i)
+
+    for i in update_files_slug:
+        doc_detail = get_doc_detail(repo_id, i['slug'])
+        doc_id = str(doc_detail['data']['id'])
+        body = ''
+        with open(i['file_path'], 'r') as f:
+            body = f.read()
+        resp = update_doc(repo_id, doc_id, i['title'], body, slug = i['slug'], public = 0)
+        print('Update doc: ', i['title'], resp)
+
+
